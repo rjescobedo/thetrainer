fc408073464cea2dd57954c35fb87799c0c1bbc5
Raul Escobedo <rjescobedo@gmail.com>
2022-01-09 12:14:55 -0600

    Foot FA Icons

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -6,19 +6,19 @@ function Footer(props) {
             <div className="container">
                 <div className="row text-center">
                     <div className="col">
-                        <a className="btn btn-social-icon btn-facebook" href="https://www.facebook.com" target="_blank">
+                        <a className="btn btn-social-icon btn-custom" href="https://www.facebook.com" target="_blank">
                             <i className="fa fa-facebook"></i>
                         </a>{' '}
-                        <a className="btn btn-social-icon btn-twitter" href="https://www.twitter.com" target="_blank">
+                        <a className="btn btn-social-icon btn-custom" href="https://www.twitter.com" target="_blank">
                             <i className="fa fa-twitter"></i>
                         </a>{' '}
-                        <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/raul-escobedo-8aa7a522/" target="_blank">
+                        <a className="btn btn-social-icon btn-custom" href="https://www.linkedin.com/in/raul-escobedo-8aa7a522/" target="_blank">
                             <i className="fa fa-linkedin"></i>
                         </a>{' '}
-                        <a className="btn btn-social-icon btn-instagram" href="https://www.instagram.com" target="_blank">
+                        <a className="btn btn-social-icon btn-custom" href="https://www.instagram.com" target="_blank">
                             <i className="fa fa-instagram"></i>
                         </a>{' '}
-                        <a className="btn btn-social-icon btn-github" href="https://github.com/rjescobedo/thetrainer.git" target="_blank">
+                        <a className="btn btn-social-icon btn-custom" href="https://github.com/rjescobedo/thetrainer.git" target="_blank">
                             <i className="fa fa-github"></i>
                         </a>{' '}
                     </div>
